28fadb96dcc6d6695dc028c26ec05ce8fe93ced0
matt-lindsay <matt_epilp@hotmail.com>
2015-07-09 20:33:47 +0100

    Add Bike class.

---
+++ bikeStuff/bikeStuff/Models/Bike.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bikeStuff.Models
+{
+    public class Bike
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
